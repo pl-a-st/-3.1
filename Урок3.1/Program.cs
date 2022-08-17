06b724f9ca10ab6e188774db72b167bf186ca961
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-08-17 18:12:59 +0300

    Добавьте файлы проекта.

---
+++ Урок3.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок3._1 {
+    class Program {
+        static void Main(string[] args) {
+
+            double first;
+            double second;
+            while (true) {
+                Console.Write("Введите первое число: ");
+                if (double.TryParse(Console.ReadLine(), out double number)) {
+                    first = number;
+                    break;
+                }
+                Console.WriteLine("Внимание, введено не число!");
+            }
+            while (true) {
+                Console.Write("Введите второе число: ");
+                if (double.TryParse(Console.ReadLine(), out double number)) {
+                    second = number;
+                    break;
+                }
+                Console.WriteLine("Внимание, введено не число!");
+            }
+            if (first > second) {
+                Console.WriteLine("Первое число больше второго.");
+            }
+            else {
+                if (second > first) {
+                    Console.WriteLine("Второе число больше первого.");
+                }
+                else {
+                    Console.WriteLine("Числа равны");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
